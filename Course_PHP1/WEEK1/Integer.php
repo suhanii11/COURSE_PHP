7936ecd28e437d0bfa0700613e9cfe25730b26b1
suhanii11 <suhaniiabdi1@gmail.com>
2026-09-18 22:35:21 +0300

    Add files via upload

--- Course_PHP1/WEEK1/Integer.php
+++ Course_PHP1/WEEK1/Integer.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+ <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -6,10 +6,21 @@
     <title>Document</title>
 </head>
 <body>
-    <p>use to Integer</p>
+
+    <p>Use of Integer</p>
+
     <?php
-    $age= 20;
-    $year = 2026   
+    $age = 20;
+    $year = 2026;
+
+    echo "My age is: ";
+    echo $age;
+
+    echo "<br>";
+
+    print "Year: ";
+    print $year;
+    ?>
 
 </body>
 </html>
